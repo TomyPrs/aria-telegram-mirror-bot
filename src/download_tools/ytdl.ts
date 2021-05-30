29262239ae8d42988f286b072b28eb765a56109d
Hrutvik Jagtap <hrutvikjagtap51@gmail.com>
2021-05-30 12:45:20 +0530

    Add support for extracting encrypted rar files
    * command: .unzipMirror <link> <password>
    
    Signed-off-by: TomyPrs <tomy.prs25@gmail.com>

--- src/download_tools/ytdl.ts
+++ src/download_tools/ytdl.ts
@@ -30,6 +30,7 @@ const dlDetails: DlVars = {
     startTime: 0,
     lastUploadCheckTimestamp: 0,
     isExtracting: false,
+    cmdString: '',
     extractedFileName: '',
     extractedFileSize: ''
 };
