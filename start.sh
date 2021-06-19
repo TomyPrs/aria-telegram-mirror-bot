6924653f6c32f6bb12c124e31c826b0e635053e3
TomyPrs <tomy.prs25@gmail.com>
2021-06-19 09:40:17 +0700

    Start.sh: Make more readable
    Signed-off-by: TomyPrs <tomy.prs25@gmail.com>

--- start.sh
+++ start.sh
@@ -2,33 +2,57 @@
 
 # To set a different value other than default(5), remove `#` from below line and replace the value
 MAX_CONCURRENT_DOWNLOADS=15
+TMP_DIR=tmp
+SERVICE_ACC="$TMP_DIR/accounts/*.json"
+CONST_FILE="$TMP_DIR/.constants.js"
+CREDS_JSON="$TMP_DIR/client_secret.json"
 
 # Check if bot is deployed to heroku
 if [[ -n $DYNO ]]; then
 
     if [[ -n $CREDS_REPO ]]; then
         echo "Creds repo Detected, Clonning.."
-        git clone $CREDS_REPO" tmp/
-        mv -v tmp/accounts /app/accounts
-        mv -v tmp/credentials.json client_secret.json /app/
-        cp tmp/.constants.js /app/
-        cp tmp/.constants.js /app/out/.constants.js
-        echo "Configuration all set"
-		rm -rf tmp/
-	else
-		exit 0
-	fi
-fi
+        git clone "$CREDS_REPO" $TMP_DIR
+    else
+        echo "You need provided CREDS REPO in repo secret! exit.."
+        exit 0
+        rm -rf "$TMP_DIR"
+    fi
 
+    if compgen -G $SERVICE_ACC > /dev/null; then
+    echo "Service account Files exist"
+    cp -r "$TMP_DIR/accounts" "$(pwd)/accounts"
+    fi
+
+    if [[ -f $CREDS_JSON ]]; then
+        echo "Credentials file detected.. Moving.."
+        mv -v tmp/credentials.json client_secret.json "$(pwd)"
+    fi
+
+    if [[ -f $CONST_FILE ]]; then
+        echo "Bot configuration set.."
+        cp tmp/.constants.js "$pwd"
+    else
+        echo "Read heroku deploy properly.."
+        exit 0
+    fi
+
+    if [[ ! -f $CREDS_JSON && -f $SERVICE_ACC ]]; then
+        echo "one of them must exist"
+        exit 0
+        rm -rf "$TMP_DIR"
+    fi
+    rm -rf $TMP_DIR; echo "Clearing $TMP_DIR"
+fi
 
 if [[ -n $MAX_CONCURRENT_DOWNLOADS ]]; then
-	sed -i'' -e "/max-concurrent-downloads/d" $(pwd)/aria.conf
-	echo -e "max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS" >> $(pwd)/aria.conf
+	sed -i'' -e "/max-concurrent-downloads/d" "$(pwd)/aria.conf"
+	echo -e "max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS" >> "$(pwd)/aria.conf"
 fi
 
-sed -i'' -e "/bt-tracker=/d" $(pwd)/aria.conf
+sed -i'' -e "/bt-tracker=/d" "$(pwd)/aria.conf"
 tracker_list=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk '$1' | tr '\n' ',' | cat`
-echo -e "bt-tracker=$tracker_list" >> $(pwd)/aria.conf
+echo -e "bt-tracker=$tracker_list" >> "$(pwd)/aria.conf"
 
 # Remove the .bak file got created from above sed
 test -f $(pwd)/aria.conf-e && rm $(pwd)/aria.conf-e
